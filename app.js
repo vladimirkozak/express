28ef188b49299a0b1d5b2bcd8a2e6d48c1fc2d2c
vladimir kozak <v.kozak@sotbit.ru>
2024-05-31 17:17:59 +0300

    minor change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<p>Page not found</p>');
+  res.status(404).send(JSON.stringify({message: 'Page not found'}));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/product', (req, res, next) => {
-  res.send('<p>add-product</p>');
-});
-
-router.get('/', (req, res, next) => {
-  res.send('<h1>hello</h1>');
+  res.send(JSON.stringify({message: {a: 1}}));
 });
 
 router.post('/add-product', (req, res, next) => {
